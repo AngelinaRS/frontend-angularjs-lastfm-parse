fa2895337e39818cd98cee27914b1e71f20fa424
AngelinaRS <candy26y_@hotmail.com>
2016-02-19 10:26:42 -0600

    Copy files of the previous project

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // Javascript Code.
-angular.module('app', [])
-.controller('PasswordController', function PasswordController($scope) {
+var app = angular.module('app', []);
+
+app.controller('PasswordController', function PasswordController($scope) {
   $scope.title = "This is AngularJS and Jasmin!";
   $scope.password = '';
   $scope.grade = function() {
@@ -13,4 +14,16 @@ angular.module('app', [])
       $scope.strength = 'weak';
     }
   };
+});
+
+app.controller("MainController", function($scope, $http){
+  $http.get("http://ws.audioscrobbler.com/2.0/?method=chart.getTopTracks&api_key=da38c0fa01ea26827dd79dcd3457804a&format=json")
+    .success(function(data){
+      $scope.datatracks = data["tracks"]["track"]; //takes the information from json
+    })
+    .error(function(err){
+      return err;
+    });
+
+    $scope.To_Order = "artist.name"; //Variable to order the data
 });
